94f6038773fc80f92a726a1f678c35e980a79282
JIWON <ziwon16@gmail.com>
2020-02-04 00:00:50 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 
-function Movie({ fav }) {
-  return <h2>I like {fav}</h2>;
+function Movie({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const movieILike = [
+  {
+    name: "Mad Max",
+    image:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video128/v4/6d/0c/83/6d0c83ff-8d14-4271-7a36-dd647ad66d21/pr_source.jpg/268x0w.jpg"
+  },
+  {
+    name: "Wall-E",
+    image: "https://t1.daumcdn.net/cfile/tistory/271BA84755A93CD107"
+  },
+  {
+    name: "Moana",
+    image:
+      "https://straightfromamovie.com/wp-content/uploads/2016/12/Moana-Movie-2016-wide.jpg"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Movie fav="Mad Max" />
-      <Movie fav="Moana" />
-      <Movie fav="Wall-e" />
-      <Movie fav="Spider-man" />
+    <div>
+      {movieILike.map(dvd => (
+        <Movie name={dvd.name} picture={dvd.image} />
+      ))}
     </div>
   );
 }
