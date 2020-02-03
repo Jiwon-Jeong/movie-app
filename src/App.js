2e0ddcf85da3e6172445ca7a066cd8589ef8f604
JIWON <ziwon16@gmail.com>
2020-02-04 00:11:42 +0900

    Fix key props Error

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ function Movie({ name, picture }) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const movieILike = [
   {
+    id: 1,
     name: "Mad Max",
     image:
       "https://is5-ssl.mzstatic.com/image/thumb/Video128/v4/6d/0c/83/6d0c83ff-8d14-4271-7a36-dd647ad66d21/pr_source.jpg/268x0w.jpg"
   },
   {
+    id: 2,
     name: "Wall-E",
     image: "https://t1.daumcdn.net/cfile/tistory/271BA84755A93CD107"
   },
   {
+    id: 3,
     name: "Moana",
     image:
       "https://straightfromamovie.com/wp-content/uploads/2016/12/Moana-Movie-2016-wide.jpg"
@@ -30,7 +33,7 @@ function App() {
   return (
     <div>
       {movieILike.map(dvd => (
-        <Movie name={dvd.name} picture={dvd.image} />
+        <Movie key={dvd.id} name={dvd.name} picture={dvd.image} />
       ))}
     </div>
   );
