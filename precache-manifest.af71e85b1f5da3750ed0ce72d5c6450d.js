c533a5eab34b821bb34c821dc9111f528615b455
JIWON <ziwon16@gmail.com>
2020-02-06 00:00:04 +0900

    Updates

---
+++ precache-manifest.af71e85b1f5da3750ed0ce72d5c6450d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93127d7cea9b971f9a29ff0ada366ad",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "99ba0de3038c0ab5c937",
+    "url": "/movie-app/static/css/main.44915168.chunk.css"
+  },
+  {
+    "revision": "a87022dd30cb7c4e609a",
+    "url": "/movie-app/static/js/2.8723044b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-app/static/js/2.8723044b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ba0de3038c0ab5c937",
+    "url": "/movie-app/static/js/main.e493f402.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
